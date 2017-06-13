fc4d08b1ce28c49100571c718b278806b3aa27f4
Pedro Paulo <ppomsg@gmail.com>
2017-06-13 22:49:35 +0000

    11 - Criação do metodo postConsumoAmbiente

--- src/Consumo.hs
+++ src/Consumo.hs
@@ -50,3 +50,15 @@ postConsumoR = do
             
 fazConsumo :: Day -> Double -> PrecoId -> AmbienteId -> Consumo
 fazConsumo dt qt pr amb = Consumo dt qt pr amb
+
+getConsumoCasaR :: Handler Html
+getConsumoCasaR = do
+        consumoCasa <- selectConsumoCasa -- isso aqui é Double
+        casaId <- selectCasaId -- Casa ID
+        casa     <- runDB $ get404 casaId
+        preco <- selectPreco -- Double
+        defaultLayout $ do
+           addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+           addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+           addScriptRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+           widgetVisuConsumo casa preco consumoCasa
